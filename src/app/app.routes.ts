a45f99739471098685def3554335e545019aabea
Aziel Hidalgo <Aziel.Hidalgo110@gmail.com>
2026-08-14 15:39:21 -0600

    elaboracion del home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'; // Ajusta la ruta según tu estructura de carpetas
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ruta predeterminada (Página de inicio)
+  { path: '', component: HomeComponent },
+  
+  // Opcional: redirección o ruta explícita /home
+  { path: 'home', component: HomeComponent },
+
+  // Comodín para redirigir si escriben una URL inexistente
+  { path: '**', redirectTo: '' }
+];
