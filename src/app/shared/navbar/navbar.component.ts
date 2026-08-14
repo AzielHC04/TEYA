3e0c3501dd405ff8d3644d2be0b11176c62f7f08
Aziel Hidalgo <Aziel.Hidalgo110@gmail.com>
2026-08-14 16:24:07 -0600

    restauracuion del navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    RouterModule // Habilita las directivas routerLink y routerLinkActive
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
   menuOpen: boolean = false;
-  dropdownOpen: boolean = false;
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
-  }
-
-  toggleDropdown(): void {
-    this.dropdownOpen = !this.dropdownOpen;
   }
 
 }
